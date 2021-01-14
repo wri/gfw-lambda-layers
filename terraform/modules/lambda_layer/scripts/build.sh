6214c4ca080179f6cf7cfff8189abb90373832a2
jterry64 <justin.terry@wri.org>
2021-01-14 13:38:44 -0500

    Remove git SHA from lambda layer image names

--- terraform/modules/lambda_layer/scripts/build.sh
+++ terraform/modules/lambda_layer/scripts/build.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-GIT_SHA="$(git rev-parse HEAD | cut -c 1-8)"
-
 LAYER_PATH=${1}
 IMAGE="globalforestwatch/${2}"
 
 pushd "${LAYER_PATH}"
 
 echo "BUILD image ${IMAGE}"
-docker build --no-cache -t "${IMAGE}":"${GIT_SHA}"  .
+docker build --no-cache -t "${IMAGE}"  .
 
 echo "CREATE container"
-docker run --name lambda_layer -itd "${IMAGE}":"${GIT_SHA}" /bin/bash
+docker run --name lambda_layer -itd "${IMAGE}" /bin/bash
 
 echo "COPY ZIP package to host"
 docker cp lambda_layer:/opt/layer.zip layer.zip
