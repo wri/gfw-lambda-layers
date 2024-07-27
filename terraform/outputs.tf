e0d65f0e00e00ae749a0293e2d11685530b01dfd
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-07-27 11:49:47 -0400

    Slim down, break out layers

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,17 @@
+output "py310_geopandas_0144_arn" {
+  value = module.py310_geopandas_0144.layer_arn
+}
+
 output "py310_mercantile_121_arn" {
   value = module.py310_mercantile_121.layer_arn
 }
 
-output "py310_pandas_153_arn" {
-  value = module.py310_pandas_153.layer_arn
+output "py310_numpy_1264_arn" {
+  value = module.py310_numpy_1264.layer_arn
 }
 
-output "py310_pandas_214_arn" {
-  value = module.py310_pandas_214.layer_arn
+output "py310_pandas_153_arn" {
+  value = module.py310_pandas_153.layer_arn
 }
 
 output "py310_pillow_950_arn" {
@@ -16,6 +20,10 @@ output "py310_pillow_950_arn" {
 
 output "py310_rasterio_138_arn" {
   value = module.py310_rasterio_138.layer_arn
+}
+
+output "py310_rasterio_1310_arn" {
+  value = module.py310_rasterio_1310.layer_arn
 }
 
 output "py310_shapely_185_arn" {
