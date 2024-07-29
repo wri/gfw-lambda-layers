93bee99466fe56b8fe832a216a43ac1aa254d8ea
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-07-29 12:32:26 -0400

    Add geopandas, numpy layers

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,17 @@
+output "py310_geopandas_0144_arn" {
+  value = module.py310_geopandas_0144.layer_arn
+}
+
 output "py310_mercantile_121_arn" {
   value = module.py310_mercantile_121.layer_arn
 }
 
-output "py310_pandas_153_arn" {
-  value = module.py310_pandas_153.layer_arn
+output "py310_numpy_1264_arn" {
+  value = module.py310_numpy_1264.layer_arn
 }
 
-output "py310_pandas_214_arn" {
-  value = module.py310_pandas_214.layer_arn
+output "py310_pandas_153_arn" {
+  value = module.py310_pandas_153.layer_arn
 }
 
 output "py310_pillow_950_arn" {
